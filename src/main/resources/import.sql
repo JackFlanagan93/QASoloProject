db6cfa0a15c45dffbdc8527cf0163a99841666c7
JackFlanagan93 <jack.flanagan93@outlook.com>
2019-06-12 08:35:33 +0100

    first commit, project structure complete

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO ACCOUNT (id, firstName, lastName , accountNumber) VALUES (1234, 'John', 'Doe', 1234);
+INSERT INTO ACCOUNT (id, firstName, lastName , accountNumber) VALUES (1235, 'Jane', 'Doe', 1235);
+INSERT INTO ACCOUNT (id, firstName, lastName , accountNumber) VALUES (1236, 'Jim', 'Taylor', 1236)
+
