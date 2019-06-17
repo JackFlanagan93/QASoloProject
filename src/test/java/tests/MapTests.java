2d5ced7370953811fa7dfb4af82beb45b06195f6
JackFlanagan93 <jack.flanagan93@outlook.com>
2019-06-17 09:23:38 +0100

    implemented Riders Map Repository and starting test

---
+++ src/test/java/tests/MapTests.java
@@ -0,0 +1,45 @@
+package tests;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.inject.Inject;
+import javax.persistence.Column;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import com.bae.persistence.domain.Riders;
+import com.bae.persistence.repository.Riders_Interface;
+import com.bae.util.JSONUtil;
+
+public class MapTests {
+	
+	@Inject
+	JSONUtil util;
+	@Inject
+	Riders_Interface Riders;
+	
+	@Before
+	public void setup() {
+		Map<Integer, Riders> ridersMap = new HashMap<Integer, Riders>();
+		
+	}
+
+	@Test
+	@Ignore
+	public void createRider() {
+		assertEquals("Rider Has Been Succesfully Added To The System", Riders.createRider("{\"riderID\":55, \"firstName\":\"Jack\", \"lastName\":\"Flanagan\", \"riderNumber\":55, \"riderTeamID\":55 ") );
+				
+	}
+
+	@Test
+	@Ignore
+	public void getRiders() {
+
+	}
+	
+}
