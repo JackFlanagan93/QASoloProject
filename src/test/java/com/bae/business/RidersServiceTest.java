f7ac3f462ac0240df137d95c391b29536bbe26d5
Jack_Flanagan <jack.flanagan93@outlook.com>
2019-06-19 20:59:05 +0100

    business unit testing started

---
+++ src/test/java/com/bae/business/RidersServiceTest.java
@@ -0,0 +1,9 @@
+package com.bae.business;
+
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class RidersServiceTest {
+
+}
