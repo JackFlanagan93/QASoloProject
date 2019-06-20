d67dd74ae506f5d215a2e48b2b4fc1d7e6e23b41
Jack_Flanagan <jack.flanagan93@outlook.com>
2019-06-20 19:49:55 +0100

    added teams button

--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -58,3 +58,43 @@ function getAllRiders() {
             }
         })
 }
+
+function getAllTeams() {
+    let searchURL = url + "Riders/GetTeams"
+
+    poll("GET", searchURL)
+
+        .then(res => {
+
+            const tableRow = document.createElement("tr");
+            let table1 = document.getElementById("table");
+
+            let resParse = JSON.parse(res);
+            let length = resParse.length;
+         
+            for (let i = 0; i < length; i++) {
+                
+                const tableRow = document.createElement("tr");
+                const td1 = document.createElement("td");
+                const td2 = document.createElement("td");
+                const td3 = document.createElement("td");
+                const td4 = document.createElement("td");
+                const td5 = document.createElement("td");
+
+                td1.innerText = resParse[i].riderID;
+                td2.innerText = resParse[i].firstName;
+                td3.innerText = resParse[i].lastName;
+                td4.innerText = resParse[i].riderNumber;
+                td5.innerText = resParse[i].riderTeamID;
+
+                tableRow.appendChild(td1);
+                tableRow.appendChild(td2);
+                tableRow.appendChild(td3);
+                tableRow.appendChild(td4);
+                tableRow.appendChild(td5);
+
+                table1.appendChild(tableRow);
+
+            }
+        })
+}
