296f52a54e72cf649522eb9df687c349b4dab45a
JackFlanagan93 <jack.flanagan93@outlook.com>
2019-06-21 10:06:43 +0100

    adjusting path jenkins file

--- Jenkinsfile
+++ Jenkinsfile
@@ -26,7 +26,7 @@ pipeline{
                         steps{
                                 sh "cd /"
 				sh "pwd"
-                                sh "sudo cp /var/lib/jenkins/workspace/DeploymentPipeline/target/MotoGPManager.war /var/lib/wildfly-10.1.0.Final/standalone/deployments/"
+                                sh "sudo cp /var/lib/jenkins/workspace/${JOB_NAME}/target/MotoGPManager.war /var/lib/wildfly-10.1.0.Final/standalone/deployments/"
                         }
                 }
                 stage('--Email--'){
---
+++ src/main/webapp/modify.js
@@ -0,0 +1,21 @@
+let url = "http://35.234.158.222:8888/MotoGPManager/start/"
+
+const poll = (url, method, body) => {
+
+    return new Promise(
+        function (res, rej) {
+            const req = new XMLHttpRequest();
+            req.onload = () => {
+                if (req.status === 200) {
+                    res(req.response);
+                } else {
+                    const reason = new Error('Rejected');
+                    rej(reason);
+                }
+            }
+            req.open(url, method)
+            req.send(body);
+        }
+    );
+}
+
